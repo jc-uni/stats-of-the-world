2e0058bd1ae36f61ecd69d23c6e309feb84e6ef6
jc-uni <jonas.claesson@unicus.com>
2021-12-07 13:46:14 +0100

    connection established

--- World_statistics.py
+++ World_statistics.py
@@ -1,7 +1,22 @@
+import mysql.connector
 
+def connect():
+    i = 0
+    connection = mysql.connector.connect(host = "localhost", user = "root", password = "MySQLPassword", database = "world")
+    print(connection)
+
+    mycursor = connection.cursor()
+    mycursor.execute("SELECT * FROM city")
+
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+#def query():
 
 def main():
     print("Hello world")
+    connect()
 
 if __name__ == "__main__":
     main()
