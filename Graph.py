39231e9ce6cc2849263dc1039bb84d1f5d8a5d84
jc-uni <jonas.claesson@unicus.com>
2021-12-09 10:16:21 +0100

    backtracked a bit

--- Graph.py
+++ Graph.py
@@ -1,20 +1,10 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-import mysql.connector
+
 
 from Analysis import Statistics
 
 class Plotting():
-
-    def __init__(self):
-        fetching_and_plotting()
-
-        #fetched_data = cursor.fetchall()
-
-        #calculated_result = Stats.median_city_pop(fetched_data)
-        #for x in calculated_result:
-            #print(x)
-
 
     def show_graph(self):
         plt.show()
@@ -26,26 +16,3 @@ class Plotting():
     def plot_medians(self, med_pop_city, med_pop_country):
         np_avg = np.array([med_pop_city, med_pop_country])
         plt.bar(["MEDIAN POP CITY","MEDIAN POP COUNTRY"],np_avg)
-        
-    def fetching_and_plotting():
-        connection = mysql.connector.connect(host = "localhost", user = "read_only_user", password = "123", database = "world")
-        print(connection)
-        cursor = connection.cursor()
-
-        Stats = Statistics()
-
-        cursor.execute(Stats.median_city_pop_query)
-        fetched_city_pop = cursor.fetchall()
-
-        cursor.execute(Stats.median_country_pop_query)
-        fetched_country_pop = cursor.fetchall()
-
-        plot_averages(fetched_city_pop,fetched_country_pop)
-
-        cursor.execute(Stats.avg_city_pop_query)
-        fetched_city_pop_avg = cursor.fetchall()
-
-        cursor.execute(Stats.avg_country_pop_query)
-        fetched_country_pop_avg = cursor.fetchall()
-
-        plot_medians(fetched_city_pop_avg, fetched_country_pop_avg)
--- World_statistics.py
+++ World_statistics.py
@@ -1,11 +1,33 @@
+import mysql.connector
 from Graph import Plotting
+from Analysis import Statistics
+
+connection = mysql.connector.connect(host = "localhost", user = "read_only_user", password = "123", database = "world")
+print(connection)
+cursor = connection.cursor()
+
 
 def main():
-    print("Hello world")
-    plot = Plotting()
-    plot.plot_averages()
-    plot.plot_medians()
-    plot.show_graph()
+    Stats = Statistics()
+    Plots = Plotting()
+
+    cursor.execute(Stats.median_city_pop_query)
+    fetched_city_pop = cursor.fetchall()
+
+    cursor.execute(Stats.median_country_pop_query)
+    fetched_country_pop = cursor.fetchall()
+
+    Plots.plot_averages(fetched_city_pop,fetched_country_pop)
+
+    cursor.execute(Stats.avg_city_pop_query)
+    fetched_city_pop_avg = cursor.fetchall()
+
+    cursor.execute(Stats.avg_country_pop_query)
+    fetched_country_pop_avg = cursor.fetchall()
+
+    Plots.plot_medians(fetched_city_pop_avg, fetched_country_pop_avg)
+
+    Plots.show_graph()
     
 
 if __name__ == "__main__":
