060e177a37261b75c0f8c04f0740772acf955849
jc-uni <jonas.claesson@unicus.com>
2021-12-15 09:07:40 +0100

    cleanup in main()

--- World_statistics.py
+++ World_statistics.py
@@ -2,36 +2,41 @@ import mysql.connector
 from Graph import Plotting
 from Analysis import Statistics
 
+Stats = Statistics()
+Plots = Plotting()
+
 connection = mysql.connector.connect(host = "localhost", user = "read_only_user", password = "123", database = "world")
 print(connection)
 cursor = connection.cursor()
 
-
-def main():
-    Stats = Statistics()
-    Plots = Plotting()
-
+def find_average_pop_country_city():
     #Average population of cities & countries
-    cursor.execute(Stats.avg_city_pop_query)
-    fetched_city_pop = cursor.fetchall()
+    fetched_city_pop = fetch_data(Stats.avg_city_pop_query)
     final_result_avg_city_pop = Stats.average_value(fetched_city_pop)
 
-    cursor.execute(Stats.avg_country_pop_query)
-    fetched_country_pop = cursor.fetchall()
+    fetched_country_pop = fetch_data(Stats.avg_country_pop_query)
     final_result_avg_country_pop = Stats.average_value(fetched_country_pop)
 
     Plots.plot_averages(final_result_avg_city_pop,final_result_avg_country_pop)
-    
-    #Median population of cities & countries
-    cursor.execute(Stats.median_city_pop_query)
-    fetched_city_pop_med = cursor.fetchall()
-    final_result_med_city_pop = Stats.median_value(fetched_city_pop_med)
 
-    cursor.execute(Stats.median_country_pop_query)
-    fetched_country_pop_med = cursor.fetchall()
+def find_median_pop_country_city():
+    #Median population of cities & countries
+    fetched_city_pop_med = fetch_data(Stats.median_city_pop_query)
+    final_result_med_city_pop = Stats.median_value(fetched_city_pop_med)
+    
+    fetched_country_pop_med = fetch_data(Stats.median_country_pop_query)
     final_result_med_country_pop = Stats.median_value(fetched_country_pop_med)
 
     Plots.plot_medians(final_result_med_city_pop, final_result_med_country_pop)
+
+def fetch_data(sql_query):
+    cursor.execute(sql_query)
+    fetched_data = cursor.fetchall()
+    return fetched_data
+
+def main():
+    find_average_pop_country_city()
+    find_median_pop_country_city()
     
     #Show average & median population of cities & countries
     Plots.show_graph()
